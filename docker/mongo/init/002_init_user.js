086d8fbb5e250d9f01bc577abdfdfec8961f3261
Asato Nago <nago@tam-bourine.co.jp>
2020-01-04 17:15:35 +0900

    fix(mongo label name n folder):

---
+++ docker/mongo/init/002_init_user.js
@@ -0,0 +1,19 @@
+let testdb = db.getSiblingDB('test');
+testdb.createUser(
+  {
+    user: 'test',
+    pwd: 'password',
+    roles:
+      [
+        {
+          role: 'root',
+          db: 'admin'
+        },
+        {
+          role: 'dbOwner',
+          db: 'test'
+        }
+      ]
+  }
+);
+testdb.getUsers();
