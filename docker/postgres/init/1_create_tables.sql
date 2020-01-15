81ada4f08ef88e3d303402cb349dbc9b2cabca0a
Asato Nago <nago@tam-bourine.co.jp>
2020-01-16 00:19:21 +0900

    fix: remove postgres

--- docker/postgres/init/1_create_tables.sql
+++
@@ -1,8 +0,0 @@
-create schema local;
-
-create table local.users(
-  id serial primary key,
-  username varchar(50) unique not null,
-  password varchar(50) not null,
-  email varchar(255) unique not null
-);
